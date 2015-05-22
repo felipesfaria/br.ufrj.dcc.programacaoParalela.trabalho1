54cc9dbedc6fedff2b791a275e11b18c15152ff1
Felipe Faria <felipesfaria@gmail.com>
2015-05-22 10:45:56 -0300

    Added Bash

---
+++ Trabalho1/bash.sh
@@ -0,0 +1,10 @@
+./T1ProgParFelipe -P -S -T -c 16 >> tabela.out
+echo 1/5
+./T1ProgParFelipe -P -S -T -c 16 >> tabela.out
+echo 2/5
+./T1ProgParFelipe -P -S -T -c 16 >> tabela.out
+echo 3/5
+./T1ProgParFelipe -P -S -T -c 16 >> tabela.out
+echo 4/5
+./T1ProgParFelipe -P -S -T -c 16 >> tabela.out
+echo 5/5
